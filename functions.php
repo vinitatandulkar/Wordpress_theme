e60bb7618a8665721b81aa89f9f66da677cbbdd4
Vinita Tandulkar <vinitatandulkar29@gmail.com>
2020-08-26 01:08:32 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Vintage functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Vintage
+ * @since 1.0.0
+ */

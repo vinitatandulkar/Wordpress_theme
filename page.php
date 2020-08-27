1013fb1add4459b5ca31077622e67b0797099fbe
Vinita Tandulkar <vinitatandulkar29@gmail.com>
2020-08-28 01:33:19 +0530

    Create page.php

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for displaying comments
+ *
+ * This is the templates that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress 
+ * @subpackage Vintage
+ * @since Vintage 1.0
+ */

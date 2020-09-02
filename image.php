abe92d0068f452ad2cd2dab64eac2663d78dc237
Vinita Tandulkar <vinitatandulkar29@gmail.com>
2020-09-02 20:42:27 +0530

    Update image.php

--- image.php
+++ image.php
@@ -3,7 +3,7 @@
  * The template for displaying comments
  *
  * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
+ * and the comment forms.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
